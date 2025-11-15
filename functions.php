a46ddb4cf56d5dd40cf044cc33436f3186262f93
Controforme <ciao@controforme.com>
2025-11-15 21:37:02 +0100

    Basic header and footer

--- functions.php
+++ functions.php
@@ -9,12 +9,33 @@
  * 
  * @since 1.0.0
  */
-function css_style() {
-	wp_enqueue_style(
-		'css-style',
-		get_stylesheet_uri(),
-		array(),
-		wp_get_theme()->get( 'Version' )
-	);
+function style_css() {
+    wp_enqueue_style( 'my_theme_style', get_stylesheet_uri() );
 }
-add_action( 'wp_enqueue_scripts', 'css_style' );
+add_action( 'wp_enqueue_scripts', 'style_css' );
+
+/**
+ * Highlight blocks outline inside the block editor
+ * 
+ * @since 1.0.0
+ */
+function block_highlight( $editor_settings, $editor_context ) {
+    $editor_settings["styles"][] = array(
+        "css" => '.wp-block.is-hovered, .wp-block.is-selected { outline: 2px solid var(--wp-admin-theme-color); transition: 0ms;}'
+    );
+	return $editor_settings;
+}
+add_filter( 'block_editor_settings_all', 'block_highlight', 10,2 );
+
+/**
+ * Theme color selector
+ * 
+ * @since 1.0.0
+ */
+/*function theme_color_selector() {
+	?> 
+	<script>
+	</script>
+	<?php
+}
+add_action( 'wp_footer', 'theme_color_selector', 1 );*/
